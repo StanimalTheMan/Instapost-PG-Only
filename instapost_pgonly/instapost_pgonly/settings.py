6f3ccb153587bc12bb6484ea7a4bd9975e07b898
Stan Choi <sjc591@nyu.edu>
2024-01-07 11:44:50 -0500

    Initialize initial Post model

--- instapost_pgonly/instapost_pgonly/settings.py
+++ instapost_pgonly/instapost_pgonly/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j%1c8^705_21ce*q(gqa5@#$6!o0t0ufnz%+xb)r8n10xhc00'
+SECRET_KEY = 'django-insecure-nt7u5ai0it)f7!36lq&k61hcy=$=649xsnp-=d)+ysefz+t)ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'posts'
 ]
 
 MIDDLEWARE = [
